e1d7b8b9015bd9f07e7557bf166570c8ec0b6274
Carlos <thehandyowl.io@gmail.com>
2017-09-26 00:40:37 +0200

    Indentation

--- MadridShops/Controller/ViewController.swift
+++ MadridShops/Controller/ViewController.swift
@@ -159,32 +159,32 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     }
     
     // Pin items
-     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-     // Don't want to show a custom image if the annotation is the user's location.
-     guard !(annotation is MKUserLocation) else {
-     return nil
-     }
-     
-     // Better to make this class property
-     let annotationIdentifier = "AnnotationIdentifier"
-     
-     var annotationView: MKAnnotationView?
-     if let dequeuedAnnotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) {
-     annotationView = dequeuedAnnotationView
-     annotationView?.annotation = annotation
-     }
-     else {
-     annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
-     annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-     }
-     
-     if let annotationView = annotationView {
-     // Configure your annotation view here
-     annotationView.canShowCallout = true
-     annotationView.image = UIImage(named: "PinImage")
-     }
-     
-     return annotationView
-     }
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // Don't want to show a custom image if the annotation is the user's location.
+        guard !(annotation is MKUserLocation) else {
+            return nil
+        }
+        
+        // Better to make this class property
+        let annotationIdentifier = "AnnotationIdentifier"
+        
+        var annotationView: MKAnnotationView?
+        if let dequeuedAnnotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) {
+            annotationView = dequeuedAnnotationView
+            annotationView?.annotation = annotation
+        }
+        else {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        
+        if let annotationView = annotationView {
+            // Configure your annotation view here
+            annotationView.canShowCallout = true
+            annotationView.image = UIImage(named: "PinImage")
+        }
+        
+        return annotationView
+    }
     
 }
