31b9b33264eba50a2a97472e17ac971ec470d100
Carlos <thehandyowl.io@gmail.com>
2017-09-30 23:59:39 +0200

    Images cached with SDWebImage

--- MadridShops/Util/String+Image.swift
+++ MadridShops/Util/String+Image.swift
@@ -2,10 +2,23 @@
 //  MadridShops
 
 import UIKit
+import SDWebImage
 
 extension String {
     func loadImage(into imageView: UIImageView) {
         let queue = OperationQueue()
+        
+        // The new one using cache and SDWebImage
+        queue.addOperation {
+            if let url = URL(string: self){
+                OperationQueue.main.addOperation {
+                    imageView.sd_setImage(with: url, placeholderImage: #imageLiteral(resourceName: "image-not-available"), options: [.continueInBackground, .refreshCached, .highPriority, .retryFailed], completed: nil)
+                }
+            }
+        }
+        
+        // The old one without cache
+        /*
         queue.addOperation {
             if let url = URL(string: self),
                 let data = NSData(contentsOf: url),
@@ -17,5 +30,6 @@ extension String {
                 
             }
         }
+         */
     }
 }
